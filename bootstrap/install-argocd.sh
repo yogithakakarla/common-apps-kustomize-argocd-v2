2985d7481292373f01f1874df37a1c22825d9de9
yogithakakarla <40813669+yogithakakarla@users.noreply.github.com>
2024-04-12 10:53:54 +0530

    Update install-argocd.sh

--- bootstrap/install-argocd.sh
+++ bootstrap/install-argocd.sh
@@ -1,19 +1,11 @@
-# curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
-# sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
-# rm argocd-linux-amd64
+
 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-#kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-#kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
-#kubectl wait --for=condition=available deployment/argocd-dex-server -n argocd --timeout=300s
 
-#ARGOCD_SERVER=$(kubectl get svc argocd-server -n argocd -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-#ARGOCD_SECRET=$(kubectl get secret -n argocd argocd-initial-admin-secret -ojsonpath="{.data.password}" | base64 --decode)
 
-#sleep 30 
 
 # Install ArgoCD cli
 kubectl config set-context --current --namespace=argocd
@@ -28,10 +20,6 @@ else
     sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd 
     rm argocd-linux-amd64
 fi
-
-#kubectl apply -f ./bootstrap/argo-users.yaml -f ./bootstrap/argo-rbac.yaml
-#argocd login $ARGOCD_SERVER --username admin --password $ARGOCD_SECRET --insecure 
-#argocd account update-password --account admin --current-password $ARGOCD_SECRET --new-password "Test1234" 
 
 
 
